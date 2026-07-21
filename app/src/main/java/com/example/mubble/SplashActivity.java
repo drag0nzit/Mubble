8bb5df057df327cda9154c00912a02abfdd0abb2
drag0nzit <mitrofantkach@gmail.com>
2026-07-21 12:43:04 +0300

    splash

---
+++ app/src/main/java/com/example/mubble/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.mubble;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import com.example.mubble.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+        }, 1000);
+    }
+}
